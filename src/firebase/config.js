54ced239350497cbedf9417956016d9a395d48b3
said aziz <aziwazaziz2003@gmail.com>
2026-02-09 23:50:16 +0100

    Secure Firebase config using environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,15 @@
-
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyASEUgbu1hUMNiDylowS8tl7pm2IYCdF-8",
-  authDomain: "university-tracker-95639.firebaseapp.com",
-  projectId: "university-tracker-95639",
-  storageBucket: "university-tracker-95639.firebasestorage.app",
-  messagingSenderId: "909077695457",
-  appId: "1:909077695457:web:0a1f5b4c0c9cf97654a0a0",
-  measurementId: "G-90V2XEQF8R"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
